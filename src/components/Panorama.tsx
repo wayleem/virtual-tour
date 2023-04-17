5261d1b9962beb737228311f93d64699a7aca5f0
way_leem <wayleemh@gmail.com>
2023-04-17 04:41:21 -0400

    add(Panorama): reacts to panoramaURL state change

---
+++ src/components/Panorama.tsx
@@ -0,0 +1,34 @@
+import { pannellum } from '../pannellum'
+import React, { useRef, useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { VirtualTourState } from '../types'
+
+
+
+export const Panorama: React.FC = () => {
+    const panoramaURL = useSelector((state: { virtualTourReducer: VirtualTourState }) => state.virtualTourReducer.panoramaURL)
+    const { current: id } = useRef(
+        `panorama-${Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000}`
+    )
+    const viewer = useRef<any>(null)
+
+    useEffect(() => {
+        viewer.current = pannellum.viewer(id, {
+            autoLoad: true,
+            panorama: panoramaURL,
+            dynamicUpdate: true,
+            compass: false,
+            friction: 0,
+            mouseZoom: false,
+            showZoomCtrl: false,
+            showFullscreenCtrl: false,
+            type: 'equirectangular'
+        })
+
+        return () => {
+            viewer.current.destroy()
+        }
+    }, [id, panoramaURL])
+
+    return <div className="relative w-screen h-screen z-0" id={id} />
+}
