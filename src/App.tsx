460c49015ea3246c263b3727023750fb1817d0db
way_leem <wayleemh@gmail.com>
2023-04-17 04:36:31 -0400

    refactor(Map): rename to Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import Map from './components/Map'
+import Home from './components/Home'
 import Preview from './components/Preview'
-import { Panorama } from './components/VirtualTour'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store'
+import VirtualTour from './components/VirtualTour'
 
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route index element={<Map />} />
-          <Route path='/virtualtour' element={<Panorama />} />
+          <Route index element={<Home />} />
+          <Route path='/virtualtour' element={<VirtualTour />} />
         </Routes>
       </BrowserRouter>
     </Provider>
---
+++ src/components/Home.tsx
@@ -0,0 +1,38 @@
+import liMap from '../assets/img/limap.svg'
+import mark from '../assets/img/mark_icon.svg'
+import Props from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { LocationContent, PreviewState } from '../types'
+import { Arshamomaque_Preserve, DownsFarm_Preserve } from '../data'
+import Preview from './Preview'
+import * as action from '../actions'
+
+type Props = {
+    location: LocationContent
+    className: string
+}
+
+function Home() {
+    const dispatch = useDispatch()
+    function MapMarker(props: Props) {
+        function clickHandler(location: Props["location"]) {
+            console.log(location.id + " selected")
+            dispatch(action.setSelect(location))
+        }
+        return (
+            <img src={mark} onClick={() => clickHandler(props.location)} className={props.className} />
+        )
+    }
+    return (
+        <div className="relative w-screen h-screen">
+            <div className="relative z-10 overflow-hidden">
+                <img src={liMap} className="w-screen h-auto" alt="Li Map" />
+                <MapMarker location={Arshamomaque_Preserve} className={Arshamomaque_Preserve.location} />
+                <MapMarker location={DownsFarm_Preserve} className={DownsFarm_Preserve.location} />
+            </div>
+            <Preview />
+        </div>
+    )
+}
+
+export default Home
