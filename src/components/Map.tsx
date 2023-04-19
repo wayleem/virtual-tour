7ea363b3f74fa6e9d32ca4cd945248c75de9ef4b
wayleem <wayleemh@gmail.com>
2023-04-19 16:01:57 -0400

    add(Map): trail map ui changes based on map selection

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,10 +1,11 @@
 import pin from "../assets/img/location-pin.png"
-import trailMap from "../assets/img/arshamomaque_trailmap.png"
 import Props from "react-redux"
-import { start, pond, obs_tower, vernal_pool } from "../data"
-import { Panorama, VirtualTourState } from "../types"
+import { start, pond, obs_tower, vernal_pool, Arshamomaque_Preserve, DownsFarm_Preserve } from "../data"
+import { Panorama, VirtualTourState, PreviewState } from "../types"
 import { useDispatch, useSelector } from 'react-redux'
 import * as action from "../actions"
+import arshamomaque_trailmap from '../assets/img/arshamomaque_trailmap.png'
+import downs_farm_trailmap from '../assets/img/downs_farm_trailmap.png'
 
 type Props = {
     panorama: Panorama
@@ -13,6 +14,10 @@ type Props = {
 function Map() {
     const dispatch = useDispatch()
     const mapActive = useSelector((state: { virtualTourReducer: VirtualTourState }) => state.virtualTourReducer.mapActive)
+    const trailMap = useSelector((state: { virtualTourReducer: VirtualTourState }) => state.virtualTourReducer.trailMap)
+    const select = useSelector((state: { previewReducer: PreviewState }) => state.previewReducer.select)
+    console.log(trailMap)
+    console.log(select.id + " is selected")
     function Pin(props: Props) {
         function clickHandler(panorama: Props["panorama"]) {
             console.log(panorama.name + " selected")
@@ -26,13 +31,21 @@ function Map() {
     }
     return (
         <>
-            {mapActive && (
-                <div className="absolute z-10 left-[50%] top-[50%] -translate-x-[50%] -translate-y-[50%]">
+            {mapActive && select === Arshamomaque_Preserve && (
+                <div className="absolute z-10 left-[50%] top-[50%] -translate-x-[50%] -translate-y-[50%] w-[50%]">
                     <Pin panorama={start} />
                     <Pin panorama={pond} />
                     <Pin panorama={vernal_pool} />
                     <Pin panorama={obs_tower} />
-                    <img src={trailMap} className="" />
+                    <img src={arshamomaque_trailmap} className="object-fill" />
+                </div>)}
+            {mapActive && select === DownsFarm_Preserve && (
+                <div className="absolute z-10 left-[50%] top-[50%] -translate-x-[50%] -translate-y-[50%] w-[50%]">
+                    <Pin panorama={start} />
+                    <Pin panorama={pond} />
+                    <Pin panorama={vernal_pool} />
+                    <Pin panorama={obs_tower} />
+                    <img src={downs_farm_trailmap} className="object-fill" />
                 </div>)}
         </>
     )
