8e151db5ba7d699a9bf15058a0effbe17e767c62
way_leem <wayleemh@gmail.com>
2023-05-16 21:32:39 -0400

    change routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import Home from './components/Home'
 import Preview from './components/Preview'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes, useLocation } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store'
 import VirtualTour from './components/VirtualTour'
 
+
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
+          <Route path='/*' element={<Home />} />
           <Route path='/virtualtour' element={<VirtualTour />} />
-          <Route path='/*' index element={<Home />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   )
 }
